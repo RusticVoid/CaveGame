54ff9574c2413db7c58ceee2362d6982f3cb6681
RusticVoid <connorpearson942@gmail.com>
2024-10-27 17:04:38 -0400

    modified:   src/conf.lua

--- src/conf.lua
+++ src/conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.console = true                    -- Attach a console (boolean, Windows only)
-    t.window.title = "Cave Game"        -- The window title (string)
+    t.window.title = "Subterranean"        -- The window title (string)
     t.window.width = 800                -- The window width (number)
     t.window.height = 600               -- The window height (number)
     t.window.resizable = true               -- The window height (number)
